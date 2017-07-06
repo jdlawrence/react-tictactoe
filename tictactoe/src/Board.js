21445b4a94e3ae598f3acb4d6d1df041c0f2526a
jdlawrence <jamil.lawrence@gmail.com>
2017-07-06 12:47:47 -0700

    Added board state

--- tictactoe/src/Board.js
+++ tictactoe/src/Board.js
@@ -2,10 +2,38 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pieces: [
+        ['x', 'x', 'o'],
+        ['o', 'o', 'o'],
+        ['x', 'x', 'o'],
+      ]
+    };
+  }
+
   render() {
     return (
-      <div> 
+      <div >
         Welcome to my tictactoe game!
+        <table className="board"> 
+          <tr>
+            <td>
+              {this.state.pieces[0][0]} | {this.state.pieces[0][1]} | {this.state.pieces[0][2]}
+            </td>
+          </tr>
+          <tr>
+            <td>
+              {this.state.pieces[1][0]} | {this.state.pieces[1][1]} | {this.state.pieces[1][2]}
+            </td>
+          </tr>
+          <tr>
+            <td>
+              {this.state.pieces[2][0]} | {this.state.pieces[2][1]} | {this.state.pieces[2][2]}
+            </td>
+          </tr>
+        </table>
       </div>
     );
   }
