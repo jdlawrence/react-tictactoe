0915c86ca6934723e6ea8bf9bc6f742a3f7eca9c
jdlawrence <jamil.lawrence@gmail.com>
2017-07-06 15:15:04 -0700

    Board pieces change on click

--- tictactoe/src/Board.js
+++ tictactoe/src/Board.js
@@ -1,38 +1,110 @@
 import React, { Component } from 'react';
 import './App.css';
+import Square from './Square';
 
 class Board extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      pieces: [
-        ['x', 'x', 'o'],
-        ['o', 'o', 'o'],
-        ['x', 'x', 'o'],
-      ]
+      pieces: ['-', '-', '-', '-', '-', '-', '-', '-', '-'],
+      turn: 'x'
     };
+
+    this.changeSquare = this.changeSquare.bind(this);
+    this.resetBoard = this.resetBoard.bind(this);
   }
 
+  resetBoard() {
+    this.setState({
+      pieces: ['-', '-', '-', '-', '-', '-', '-', '-', '-']
+    });
+  }
+
+  changeSquare(squareNum) {
+    if (this.state.pieces[squareNum] === '-') {
+      if (this.state.turn === 'x') {
+        this.setState({
+          turn: 'o'
+        });
+      } else {
+        this.setState({
+          turn: 'x'
+        });
+      }
+      var newPieces = this.state.pieces.slice(0, squareNum)
+        .concat(this.state.turn)
+        .concat(this.state.pieces.slice(squareNum + 1));
+      this.setState({ pieces: newPieces });
+    }
+
+  }
   render() {
     return (
-      <div >
+      <div className="board">
         Welcome to my tictactoe game!
-        <table className="board"> 
-          <tr>
-            <td>
-              {this.state.pieces[0][0]} | {this.state.pieces[0][1]} | {this.state.pieces[0][2]}
-            </td>
-          </tr>
-          <tr>
-            <td>
-              {this.state.pieces[1][0]} | {this.state.pieces[1][1]} | {this.state.pieces[1][2]}
-            </td>
-          </tr>
-          <tr>
-            <td>
-              {this.state.pieces[2][0]} | {this.state.pieces[2][1]} | {this.state.pieces[2][2]}
-            </td>
-          </tr>
+        <div>
+          <button onClick={this.resetBoard}>reset the board</button>
+        </div>
+        <table className="grid">
+          <tbody>
+            <tr>
+              <td>
+                <Square
+                  num={0}
+                  piece={this.state.pieces[0]}
+                  changeSquare={this.changeSquare}
+                /> |
+                <Square
+                  num={1}
+                  piece={this.state.pieces[1]}
+                  changeSquare={this.changeSquare}
+                /> |
+                 <Square
+                  num={2}
+                  piece={this.state.pieces[2]}
+                  changeSquare={this.changeSquare}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <Square
+                  num={3}
+                  piece={this.state.pieces[3]}
+                  changeSquare={this.changeSquare}
+                /> |
+                <Square
+                  num={4}
+                  piece={this.state.pieces[4]}
+                  changeSquare={this.changeSquare}
+                /> |
+                 <Square
+                  num={5}
+                  piece={this.state.pieces[5]}
+                  changeSquare={this.changeSquare}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <Square
+                  num={6}
+                  piece={this.state.pieces[6]}
+                  changeSquare={this.changeSquare}
+                /> |
+                <Square
+                  num={7}
+                  piece={this.state.pieces[7]}
+                  changeSquare={this.changeSquare}
+                /> |
+                 <Square
+                  num={8}
+                  piece={this.state.pieces[8]}
+                  changeSquare={this.changeSquare}
+                />
+              </td>
+            </tr>
+          </tbody>
         </table>
       </div>
     );
