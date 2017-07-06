43c744105874f6b2c1df8a1c9ee5325ccdea0653
jdlawrence <jamil.lawrence@gmail.com>
2017-07-06 11:54:28 -0700

    Creates board component

---
+++ tictactoe/src/Board.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Board extends Component {
+  render() {
+    return (
+      <div> 
+        Welcome to my tictactoe game!
+      </div>
+    );
+  }
+}
+
+export default Board;
+
